6e5d062ba032cf45080fdfe530180ce609495d16
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2024-01-16 05:55:42 +0530

    Update EmailForm.js

--- src/pages/EmailForm.js
+++ src/pages/EmailForm.js
@@ -8,41 +8,36 @@ export default function EmailForm(){
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
   const [Message, setMessage] = useState(null);
-  const [file, setFile] = useState(null);
-  const onDrop = (acceptedFiles) => {
-    setFile(acceptedFiles[0]);
-  };
-  const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: '.csv' });
+  // const [file, setFile] = useState(null);
+  // const onDrop = (acceptedFiles) => {
+  //   setFile(acceptedFiles[0]);
+  // };
+  // const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: '.csv' });
   
 
-   async function SendEmail (event){
-    event.preventDefault()
-    try {
-      const formData = new FormData();
-      formData.append('email', email);
-      formData.append('Message', Message);
-      if (file) {
-        formData.append('file', file);
+  async function SendEmail() {
+  
+    try {  
+      console.log(email)
+      const response = await axios.post('http://localhost:5000/send-email', {
+        email:email,
+        Message:Message
       }
-
-      const response = await axios.post('http://localhost:5000/send-email', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
+      );
+  
       setSuccessMessage(response.data);
       setError(null);
     } catch (error) {
       setError('Error sending email');
       setSuccessMessage(null);
     }
-  };
+  }
   
+  // console.log(email)
   return (
     <div>
       <h2 className='p-5 font-semibold'>Simple Email Form</h2>
-      <form  className='mx-5' onSubmit={SendEmail}>
+      <div className='mx-5'>
         <label>Email Address:</label>
         <input
           onChange={(e)=>{
@@ -59,12 +54,12 @@ export default function EmailForm(){
           className='border-4 px-4 mx-4 py-10 rounded-lg '
 
         />
-        <div {...getRootProps()} className=' border-4 p-10 border-dotted mb-6 w-[30%] cursor-pointer'>
+        {/* <div {...getRootProps()} className=' border-4 p-10 border-dotted mb-6 w-[30%] cursor-pointer'>
           <input {...getInputProps()} />
           <p>Drag & drop or click to select a CSV file</p>
-        </div>
-        <button className=' bg-black text-white p-2 rounded-lg'  type="submit">Send Email</button>
-      </form>
+        </div> */}
+        <button  onClick={SendEmail} className=' bg-black text-white p-2 rounded-lg'  type="submit">Send Email</button>
+      </div>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
